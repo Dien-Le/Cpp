d1a3a83dd5ceca52544f0f259b0af77d95006cdf
Dien <dile2842@gmail.com>
2025-10-09 20:29:32 -0400

    finished swapping process

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -4,44 +4,64 @@ using namespace std;
 
 struct Node {
     int a; 
-    Node* b;
+    Node* next;
+    Node* prev;
 };
 
-int insertion(Node* begin_value){
+int insertion_sort(Node* begin_value){
 
     Node* cache = begin_value;
-    while (cache ->b != NULL);
-    {
-        if (cache->b->a < cache->a);
-        // Backtracking
-            
+    while (cache ->next != NULL){
+        
+        if (cache->next->a < cache->a) {
+        // Swapping stuff
+            Node* cache_next = cache->next;
+            Node* next_next = cache_next ->next;
+            Node* cache_prev = cache -> prev;
+        
+            //Changing next values
+            cache->next = cache_next;
+            cache_next->next = cache;
+
+            //Changing previous values
+            cache_next->prev = cache->prev;
+            cache->prev = cache_next;
+
+            //Changing parameters arround cache
+
+            next_next -> prev = cache;
+            cache_prev -> next = cache_next;
+
+        }
+
         else {
         // If nothing goes wrong
-            Node* prev = cache;
-            cache = cache ->b;
+            cache = cache ->next;
         }
-    }
-
+    };
     return 0;
-
 }
 
 int main() {
     Node n1, n2, n3, n4;
 
     n1.a = 3;
-    n1.b = &n2;
+    n1.next = &n2;
+    n1.prev = NULL;
 
-    n2.a = 6;
-    n2.b = &n3;
+    n2.a = 8;
+    n2.next = &n3;
+    n2.prev = &n1;
 
-    n3.a = 8;
-    n3.b = &n4;
+    n3.a = 6;
+    n3.next = &n4;
+    n3.prev = &n2;
 
     n4.a = 4;
-    n4.b = NULL;
+    n4.next = NULL;
+    n4.prev = &n3;
 
-    insertion(&n2);
+    insertion_sort(&n2);
 
     return 0;
 }
